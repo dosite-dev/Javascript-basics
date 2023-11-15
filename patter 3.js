c68909c0471dc8973699191416495ffe14cb3915
dosite-dev <dositecyiza@gmail.com>
2023-11-15 15:07:15 +0200

    pattern 3.js

---
+++ patter 3.js
@@ -0,0 +1,14 @@
+/*Create a function that displays a pattern like this:
+1 1 1 1 
+1 1 1 0  
+1 1 0 0  
+1 0 0 0  
+ */
+
+function patternSquare(a){
+    for( i=a; i>=0; i--){
+        console.log('1 '.repeat(i)+('0 ' .repeat(a-i)))
+    }
+
+}
+patternSquare(4,5);
