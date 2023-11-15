9d6d354b962763c3d965fbd0c0b1c08fd7dc18bc
dosite-dev <dositecyiza@gmail.com>
2023-11-15 15:36:09 +0200

    reversed of array.js

---
+++ reverse of array.js
@@ -0,0 +1,14 @@
+/* Given an array like this:
+var array = [1,3,4,3,7,8,0,12,19];    
+Create a function to reverse the order of the elements inside the given array.
+ */
+
+function reversedArray(){
+  
+    console.log(arr);
+    return arr.reverse();
+      
+  }
+  var arr=[1,3,4,3,7,8,0,12,19];
+  console.log(arr.reverse());
+  
