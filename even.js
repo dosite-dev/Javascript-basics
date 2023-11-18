45644e13157bc74ce894b848a5d1dade30a54982
dosite-dev <dositecyiza@gmail.com>
2023-11-18 22:23:20 +0200

    nberOfCharacter

---
+++ even.js
@@ -0,0 +1,37 @@
+/*Create a function that displays an even number between 1 and 100 */
+function evenNumber(nums){
+   for(num=1 ;num<= 100; num++){
+   }
+    if(num % 2 == 0){
+        console.log("number is even");
+    }
+    else{
+        console.log("number is not even");
+    }
+}
+
+evenNumber();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+}
