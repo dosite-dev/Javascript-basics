a69b6920be2d635082c9812f222b835b920a58af
dosite-dev <dositecyiza@gmail.com>
2023-11-20 05:21:52 +0200

    even.js

--- even.js
+++ even.js
@@ -1,37 +1,11 @@
-/*Create a function that displays an even number between 1 and 100 */
-function evenNumber(nums){
-   for(num=1 ;num<= 100; num++){
-   }
-    if(num % 2 == 0){
-        console.log("number is even");
++/* a function that displays an even number between 1 and 100
+ */
+function even(){
+    for(let i=0; i<= 100; i++){
+      if( i% 2 === 0){
+        console.log("even numbers", i);
+      }
     }
-    else{
-        console.log("number is not even");
-    }
-}
-
-evenNumber();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-}
+  }
+  even();
+  
