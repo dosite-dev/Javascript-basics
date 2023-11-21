97c561d1303d9b7730d045e45b5f247f0108cc17
dosite-dev <dositecyiza@gmail.com>
2023-11-21 12:25:32 +0200

    factorial.js

---
+++ factorial.js
@@ -0,0 +1,11 @@
+/* a function that calculates the factorial of a number*/
+function factorialOfNumber(b){
+
+        var result=1;
+        for (i=1 ;i <=5;i++){
+          result *=i;
+        }
+        return result;
+      }
+      console.log(factorialOfNumber(5));  
+
