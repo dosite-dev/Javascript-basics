30ef2abf2dcaa1ad079de8e7ee43e8969060bff0
dosite-dev <dositecyiza@gmail.com>
2023-11-22 11:35:54 +0200

    palindrome.js

---
+++ squarerootOfNumber.js
@@ -0,0 +1,9 @@
+/* a function that returns the square root of a given number */
+function squarerootOfNumber(number){
+    if (number < 0){
+      return "invalid";
+    }
+      return Math.sqrt(number);
+    }
+    
+    console.log(squarerootOfNumber(4));
